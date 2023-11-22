857b3a5f9154001f8967d2e85d57e9c158c290d8
Arsenii <senyasmurf@gmail.com>
2023-11-22 20:07:02 +0300

    D.z

--- src/Complex.java
+++ src/Complex.java
@@ -7,17 +7,37 @@ public class Complex {
         this.re = re;
         this.im = im;
     }
-    public Complex sum(Complex c1, Complex c2){ //метод сложения
+    public static Complex sum(Complex c1, Complex c2){ //метод сложения
         return new Complex(c1.re + c2.re, c1.im + c2.im);
     }
 
-    public Complex sub(Complex c1, Complex c2){ //метод вычитания
+    public static Complex sub(Complex c1, Complex c2){ //метод вычитания
         return new Complex(c1.re - c2.re, c1.im - c2.im);
     }
 
-    public Complex mult(Complex c1, Complex c2){ //метод умножения
+    public static Complex mult(Complex c1, Complex c2){ //метод умножения
         return new Complex(c1.re * c2.re - c1.im * c2.im, c1.re * c2.im + c1.im * c2.re);
     }
 
+    public static Complex div(Complex c1, Complex c2){
+        Complex chisl = Complex.mult(c1, c2.sopr());
+        double znam = Complex.mult(c2, c2.sopr()).re;
+        return new Complex(chisl.re / znam, chisl.im / znam);
+    }
+
+    public Complex sopr() {
+        return new Complex(this.re, -this.im);
+    }
+
+    public static boolean equals(Complex c1, Complex c2){
+        double rc1 = Math.sqrt(Math.pow(c1.re, 2) + Math.pow(c1.im, 2));
+        double rc2 = Math.sqrt(Math.pow(c2.re, 2) + Math.pow(c2.im, 2));
+        return rc1 == rc2;
+    }
+
+    @Override
+    public String toString(){
+        return im >= 0 ? re + "+" + im + "i": re + "" + im + "i";
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-    c
+        Complex complex1 = new Complex(1, 1);
+        Complex complex2 = new Complex(2, 2);
+        System.out.println(Complex.sum(complex1, complex2));
+        System.out.println(Complex.sub(complex1, complex2));
+        System.out.println(Complex.mult(complex1, complex2));
+        System.out.println(Complex.div(complex1, complex2));
+        System.out.println(Complex.equals(complex1, complex2));
+        System.out.println(complex2.sopr());
+    }
 }
